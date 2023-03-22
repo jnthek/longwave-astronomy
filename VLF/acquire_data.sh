ed3a8e2701929c37ad5757448bb60ae0885bae01
VU3VWB <vu3vwb@gmail.com>
2023-03-22 20:51:24 +0800

    Fix a major bug, add a waterfall section to the code

--- VLF/acquire_data.sh
+++ VLF/acquire_data.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-
-arecord -c 2 -D hw:CARD=ICUSBAUDIO7D,DEV=0 -r 48000 -vvv -f S16_LE test.wav
+./config_soundcard_linein.sh
+arecord -c 2 -D hw:CARD=ICUSBAUDIO7D,DEV=0 -r 48000 -vv -f S16_LE test.wav
